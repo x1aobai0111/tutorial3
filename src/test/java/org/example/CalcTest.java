9da941996ec9c5aad41bc07a75486d60ea954cea
x1aobai0111 <1735687568@qq.com>
2026-09-22 21:59:36 +0800

    Update CalcTest.java

--- src/test/java/org/example/CalcTest.java
+++ src/test/java/org/example/CalcTest.java
@@ -9,6 +9,10 @@ public class CalcTest {
     @Test
     void testAddition() {
         assertEquals(4, c.add(2,2));
+      @Test
+    void testSubtraction(){
+        Calc c = new Calc();
+        assertEquals(2, c.subtract(4,2));
     }
 
 }
